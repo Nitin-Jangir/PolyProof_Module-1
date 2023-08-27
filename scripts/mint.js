dbe1787ad2bae5b5c493345c936a133d02c0d00a
Nitin-Jangir <137036246+Nitin-Jangir@users.noreply.github.com>
2023-08-27 21:25:48 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -6,32 +6,32 @@ const nftData = [
   {
    "name":"NFT 1",
       "description":"This is the first NFT",
-      "image":"https://amethyst-brave-gorilla-959.mypinata.cloud/ipfs/QmT3MzQeDGKzKY7eRanb4Gmtzpvvbyjg6jVpGmjzQ4EUnG?_gl=1*1fo24jy*_ga*MzYxNDkzNDYxLjE2OTI2MDczMzY.*_ga_5RMPXG14TE*MTY5MjYzNDg1OS4yLjAuMTY5MjYzNDg1OS42MC4wLjA."
+      "image":"https://maroon-striped-wildebeest-439.mypinata.cloud/ipfs/Qmd7vVGBWt2zQJSAwekQWYHiRAZqKppGPR7B6eFBTFC7uq?_gl=1*5pw1vq*_ga*MTcyMDI2NzM5OS4xNjkyODA5OTAy*_ga_5RMPXG14TE*MTY5MjgwOTkwMi4xLjEuMTY5MjgxMDUwNi40NC4wLjA."
   },
   {
     "name":"NFT 2",
     "description":"This is the first NFT",
-    "image":"https://amethyst-brave-gorilla-959.mypinata.cloud/ipfs/QmY2MpdEshwYygrW3UbjsdzijKKyvhoCrnz9YKJVGSCwC4?_gl=1*13oap1i*_ga*MzYxNDkzNDYxLjE2OTI2MDczMzY.*_ga_5RMPXG14TE*MTY5MjcyMzU1NC4zLjAuMTY5MjcyMzU1NC42MC4wLjA."
+    "image":"https://maroon-striped-wildebeest-439.mypinata.cloud/ipfs/QmU1PunCwyDRSDw2rXVnW9Z8dXBv61zW8CVb6p8jQ1zAdr?_gl=1*5pw1vq*_ga*MTcyMDI2NzM5OS4xNjkyODA5OTAy*_ga_5RMPXG14TE*MTY5MjgwOTkwMi4xLjEuMTY5MjgxMDUwNi40NC4wLjA."
 },
 {
   "name":"NFT 3",
   "description":"This is the first NFT",
-  "image":"https://amethyst-brave-gorilla-959.mypinata.cloud/ipfs/QmT63noFdcUrvi1o5Upuuq71Jg3LUunCybXNKyXdFF6Rw2?_gl=1*13oap1i*_ga*MzYxNDkzNDYxLjE2OTI2MDczMzY.*_ga_5RMPXG14TE*MTY5MjcyMzU1NC4zLjAuMTY5MjcyMzU1NC42MC4wLjA."
+  "image":"https://maroon-striped-wildebeest-439.mypinata.cloud/ipfs/QmQnxdtUjSG4BVr9KZjYatEhKr67TivQEJeMrxpLCBHxBF?_gl=1*1fa4e08*_ga*MTcyMDI2NzM5OS4xNjkyODA5OTAy*_ga_5RMPXG14TE*MTY5MjgwOTkwMi4xLjEuMTY5MjgxMDUwNi40NC4wLjA."
 },
 {
   "name":"NFT 4",
   "description":"This is the first NFT",
-  "image":"https://amethyst-brave-gorilla-959.mypinata.cloud/ipfs/QmTnzta6S5eHCj7ehpgmbZbqBi1nZdHLxTScKrNXgCsynr?_gl=1*13oap1i*_ga*MzYxNDkzNDYxLjE2OTI2MDczMzY.*_ga_5RMPXG14TE*MTY5MjcyMzU1NC4zLjAuMTY5MjcyMzU1NC42MC4wLjA."
+  "image":"https://maroon-striped-wildebeest-439.mypinata.cloud/ipfs/QmPsweKNRg7v1tJGXXwJnqHQyi8Ef2HH8WabQ1NSGfSqyL?_gl=1*1pal6pe*_ga*MTcyMDI2NzM5OS4xNjkyODA5OTAy*_ga_5RMPXG14TE*MTY5MjgwOTkwMi4xLjEuMTY5MjgxMDUwNi40NC4wLjA."
 },
 {
   "name":"NFT 5",
   "description":"This is the first NFT",
-  "image":"https://amethyst-brave-gorilla-959.mypinata.cloud/ipfs/QmbLkenzLnYPFrn8hXLJn6dp6uQ281GYECcNqKF3oxnf1e?_gl=1*13oap1i*_ga*MzYxNDkzNDYxLjE2OTI2MDczMzY.*_ga_5RMPXG14TE*MTY5MjcyMzU1NC4zLjAuMTY5MjcyMzU1NC42MC4wLjA."
+  "image":"https://maroon-striped-wildebeest-439.mypinata.cloud/ipfs/Qmb6JhdSqoMYkRZ21xPD3EGAULdaA8YjbqAxAmSVn4mSAm?_gl=1*1pal6pe*_ga*MTcyMDI2NzM5OS4xNjkyODA5OTAy*_ga_5RMPXG14TE*MTY5MjgwOTkwMi4xLjEuMTY5MjgxMDUwNi40NC4wLjA."
 }
 
 ];
 
-const contractAddress = "0x995251311625F7eb26b58360A946b0F6D758d864";
+const contractAddress = "0x30004623bd6182C33F83Ab4b439D9D44fC3BBd99";
 const tokenABI = tokenContractJSON.abi;
 const walletAddress = "0x9BFaf3862a8f99a45B4fA2cfcaA1665a13eC4d60"; // place your public address for your wallet here
 
